cd7d6ac28a30eeaa26bd2eef640863e5fceec1db
Mateus Martins <mateusmartinspee@gmail.com>
2023-05-21 23:41:08 -0300

    feat: add graphql and graphiql to project

--- lib/orbit_web/router.ex
+++ lib/orbit_web/router.ex
@@ -5,6 +5,13 @@ defmodule OrbitWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api" do
+    pipe_through :api
+
+    forward  "/graphql", Absinthe.Plug, schema: Orbit.Schema
+    forward  "/graphiql", Absinthe.Plug.GraphiQL, schema: Orbit.Schema
+  end
+
   scope "/api", OrbitWeb do
     pipe_through :api
   end
